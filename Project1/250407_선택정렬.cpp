af5d6fd6b009e69d1f01583b61c522e1d8756b4e
Sunhee Cho <ihateundefined@gamil.com>
2025-04-07 10:42:37 +0900

    선택정렬

--- Project1/250407_선택정렬.cpp
+++ Project1/250407_선택정렬.cpp
@@ -3,38 +3,39 @@
 
 void swap(char* a, char* b)
 {
-	char temp = *a;
-	*a = *b;
-	*b = temp;
+    char temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 void SelectionSort(char* ar, int size)
 {
-	int minidx = 0;
+    int minidx = 0;
 
-	for (int i = 0; i < size - 1; i++) {
-		for (int j = 1 + i, minidx = i; j < size; j++) {
-			if (ar[minidx] > ar[j]) {
-				minidx = j;
-			}
-		}
+    for (int i = 0; i < size - 1; i++) {
+        minidx = i;
 
-		if (minidx != i)
-		{
-			swap(&ar[minidx], &ar[i]);
-		}
-	}
+        for (int j = i + 1; j < size; j++) {
+            if (ar[minidx] > ar[j]) {
+                minidx = j;
+            }
+        }
+
+        if (minidx != i) {
+            swap(&ar[i], &ar[minidx]);
+        }
+    }
 }
 
 void main()
 {
-	char str[] = "winapi";
+    char str[] = "winapi";
 
-	int len = strlen(str);
+    int len = strlen(str);
 
-	printf("정렬 전 문자열 : %s\n", str);
+    printf("정렬 전 문자열 : %s\n", str);
 
-	SelectionSort(str, len);
+    SelectionSort(str, len);
 
-	printf("정렬 완료 후 문자열 : %s\n", str);
+    printf("정렬 완료 후 문자열 : %s\n", str);
 }
